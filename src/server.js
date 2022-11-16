4c291b8961e50ba40e403fd1de7b4055a8d2fe06
Hayden Howle <hayden.howle@teambespin.us>
2022-11-16 14:05:35 -0600

    Changed standalone apollo to use apollo on top of express

--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
+import http from 'http';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import cors from 'cors';
+import pkg from 'body-parser';
 import { typeDefs, resolvers } from './index.js';
 
+const app = express();
+const { json } = pkg;
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
+await server.start();
+app.use(
+  '/graphql',
+  cors(),
+  json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token })
+  })
+);
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
