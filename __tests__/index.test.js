b59151dd503e4d2686a8effdcf539c26e6562f8a
Howler4695 <howler4695@gmail.com>
2022-11-29 13:02:59 -0600

    Everythings static

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { books, authors, categories } from '../data/schema.js';
+import { books } from '../data/schema.js';
 import newTestServer from './utils/test-server.js';
 
 describe('index', () => {
@@ -30,21 +30,27 @@ describe('index', () => {
       expect(errors).toBeUndefined();
       expect(data.getBooks).toBeDefined();
 
-      for (const [i, book] of data.getBooks.entries()) {
-        expect(book.id).toBe(books[i].id);
-        expect(book.title).toBe(books[i].title);
-        expect(book.coverImage).toBe(books[i].coverImage);
-        if (book.description) {
-          expect(book.description).toBe(books[i].description);
-        } else {
-          expect(book.description).toBeNull();
-        }
-
-        expect(book.author.id).toBe(books[i].author);
-        expect(book.categories.map(category => category.id)).toEqual(
-          books[i].categories
-        );
+      expect(data.getBooks.length).toBe(4);
+      for (const book of data.getBooks) {
+        expect(book).toBeDefined();
       }
+
+      const book = data.getBooks[0];
+
+      expect(book.id).toBe('1');
+      expect(book.title).toBe('Harry Potter and the Chamber of Secrets');
+      expect(book.author.id).toBe('1');
+      expect(book.coverImage).toBe(
+        'https://m.media-amazon.com/images/I/51mFoFmu0EL._AC_SY780_.jpg'
+      );
+      expect(book.description).toBe(
+        'Harry Potter and the Chamber of Secrets is a 1998 young adult fantasy novel by J.K. Rowling, the second in the Harry Potter series. The story follows Harry’s tumultuous second year at Hogwarts School of Witchcraft and Wizardry, including an encounter with Voldemort, the wizard who killed Harry’s parents. Against this fantastic backdrop, Rowling examines such themes as death, fame, friendship, choice, and prejudice. Upon release, the novel became a worldwide bestseller and won several awards, including Children’s Book of the Year at the British Book Awards and the Nestlé Smarties Book Award; it was subsequently adapted into a 2002 film directed by Chris Columbus.'
+      );
+
+      expect(book.categories.map(category => category.id)).toStrictEqual([
+        '1',
+        '2'
+      ]);
     });
   });
 
@@ -71,12 +77,17 @@ describe('index', () => {
       expect(errors).toBeUndefined();
       expect(data.getAuthors).toBeDefined();
 
-      for (const [i, author] of data.getAuthors.entries()) {
-        expect(author.id).toBe(authors[i].id);
-        expect(author.firstName).toBe(authors[i].firstName);
-        expect(author.lastName).toBe(authors[i].lastName);
-        expect(author.books.map(book => book.id)).toEqual(authors[i].books);
+      expect(data.getAuthors.length).toBe(8);
+      for (const author of data.getAuthors) {
+        expect(author).toBeDefined();
       }
+
+      const author = data.getAuthors[0];
+
+      expect(author.id).toBe('1');
+      expect(author.firstName).toBe('J.K.');
+      expect(author.lastName).toBe('Rowling');
+      expect(author.books.map(book => book.id)).toStrictEqual(['1', '2', '3']);
     });
   });
 
@@ -102,13 +113,20 @@ describe('index', () => {
       expect(errors).toBeUndefined();
       expect(data.getCategories).toBeDefined();
 
-      for (const [i, category] of data.getCategories.entries()) {
-        expect(category.id).toBe(categories[i].id);
-        expect(category.name).toBe(categories[i].name);
-        expect(category.books.map(book => book.id)).toEqual(
-          categories[i].books
-        );
+      expect(data.getCategories.length).toBe(3);
+      for (const category of data.getCategories) {
+        expect(category).toBeDefined();
       }
+
+      const category = data.getCategories[0];
+
+      expect(category.id).toBe('1');
+      expect(category.name).toBe('Fantasy');
+      expect(category.books.map(book => book.id)).toStrictEqual([
+        '1',
+        '2',
+        '3'
+      ]);
     });
 
     describe('getBooksByIds', () => {
@@ -134,7 +152,7 @@ describe('index', () => {
 
         const { getBooksByIds: testBookTitles } = data;
 
-        expect(testBookTitles.map(book => book.title)).toEqual([
+        expect(testBookTitles.map(book => book.title)).toStrictEqual([
           'Harry Potter and the Chamber of Secrets',
           'Harry Potter and the Prisoner of Azkaban',
           'C All in One Desk Reference For Dummies'
@@ -155,7 +173,7 @@ describe('index', () => {
 
         const { getBooksByIds: testBookTitles } = data;
 
-        expect(testBookTitles.map(book => book.title)).toEqual([
+        expect(testBookTitles.map(book => book.title)).toStrictEqual([
           'Harry Potter and the Chamber of Secrets',
           'Harry Potter and the Prisoner of Azkaban'
         ]);
@@ -210,7 +228,7 @@ describe('index', () => {
         expect(testBook.title).toBe('Starship Troopers');
         expect(testBook.author.id).toBe('2');
         expect(testBook.coverImage).toBe('https://incredible-cover-image.jpeg');
-        expect(testBook.categories.map(category => category.id)).toEqual([
+        expect(testBook.categories.map(category => category.id)).toStrictEqual([
           '1',
           '2'
         ]);
